8c42eb1fd50dd9125481b96f4d8b30f86b4df772
Rubén Lacalle Zorrilla <ruben.laczor@gmail.com>
2025-02-14 10:22:20 +0100

    2025-02-14 Inicio actividad 5

---
+++ Actividad 5 (React sin dependencias)/main.js
@@ -0,0 +1,11 @@
+import React from "https://esm.sh/react@19.0.0-beta-04b058868c-20240508/?dev";
+import ReactDOMClient from "https://esm.sh/react-dom@19.0.0-beta-04b058868c-20240508/client/?dev";
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const rootElement = ReactDOMClient.createRoot(
+        document.getElementById('root')
+    )
+
+    const nBotones = [1, 2, 3];
+    
+})
