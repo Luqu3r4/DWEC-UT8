8b37569f3b960ea5563bc86f9351c0fd3ad11d95
Rubén Lacalle Zorrilla <ruben.laczor@gmail.com>
2025-02-14 09:26:42 +0100

    2025-02-14 Realizacion del Ejercicio 4 (npm)

--- Actividad 4 (npm)/main.js
+++ Actividad 4 (npm)/main.js
@@ -1,4 +1,26 @@
-import readline  from "readline";
+import readline from "readline";
 import { stdin as input, stdout as output } from "process";
+import chalk from "chalk";
 
-const rl = readline.createInterface({input, output});
+const rl = readline.createInterface({ input, output });
+
+rl.question('¿Cual es tu nombre?', (nombre)=>{
+    rl.question('¿Cual es tu edad?', (edad)=>{
+        let mensaje = `El usuario ${nombre} tiene una edad de ${edad}.`;
+
+        switch (true) {
+            case edad < 18:
+                console.log(chalk.red(mensaje));
+                break;
+            case edad >= 18 && edad <= 65:
+                console.log(chalk.green(mensaje));
+                break;
+            case edad > 65:
+                console.log(chalk.blue(mensaje));
+                break;
+            default:
+                break;
+        }
+        rl.close()
+    })
+})
